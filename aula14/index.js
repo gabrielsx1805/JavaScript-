b410ba3e013242122bc4872e9cb575b1d8460ce5
gabrielsx1805 <94087426+gabrielsx1805@users.noreply.github.com>
2022-02-24 15:09:59 -0300

    att

---
+++ aula14/index.js
@@ -0,0 +1,8 @@
+var num = [5, 8, 4 , 5 , 6]
+
+console.log(`Nosso Vetor tem o ${num.length}`)
+let pos = num.indexOf(4)
+if(pos <= -1){
+    console.log(`O valor nao foi encontrado`)
+} else{
+console.log(`O valor 8 esta na posicao ${pos}`)}
